84f3c6c4fbf04916f91218b1cb05e5a564f94c33
Roman <82379236+ungros@users.noreply.github.com>
2022-07-25 12:00:20 +0300

    Add files via upload

---
+++ EXTNSIONS.swift
@@ -0,0 +1,109 @@
+//
+//  EXTNSIONS.swift
+//  Extensions
+//
+//  Created by Roman on 25.07.2022.
+//
+
+import UIKit
+
+//MARK: Constraints
+
+extension UIView {
+    
+    func anchorXXX(top: NSLayoutYAxisAnchor? = nil,
+                left: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeft: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingRight: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func centerX(inView view: UIView) {
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+}
+
+//MARK: Color Hex
+
+extension UIColor {
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+}
+
+//MARK: Custom Button
+
+func makeSystem(_ button: UIButton) {
+    button.addTarget(self, action: #selector(handlineIn), for: [
+        .touchDown,
+        .touchUpInside
+    ])
+    button.addTarget(self, action: #selector(handlineOut), for: [
+        .touchDragOutside,
+        .touchUpInside,
+        .touchUpOutside,
+        .touchDragExit,
+        .touchCancel
+    ])
+}
+
+@objc func handlineIn() {
+    UIView.animate(withDuration: 0.15) {self.alpha = 0.55}
+}
+
+@objc func handlineOut() {
+    UIView.animate(withDuration: 0.15) {self.alpha = 1}
+}
+
+func setupView(_ view: UIView) {
+    addSubview(view)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    }
+}
+
+
